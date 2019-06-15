af5b0c5210acda726bbce461583ca935c07fe0a3
DiegoAloslla <diegoalosilla@gmail.com>
2019-06-14 19:08:16 -0500

    :white_check_mark: modified files

---
+++ DataBase/dbo.BuisnessOwner.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[BuisnessOwner] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (70)  NOT NULL,
+    [LastName]  NVARCHAR (70)  NOT NULL,
+    [Dni]       NVARCHAR (50)  NOT NULL,
+    [Email]     NVARCHAR (200) NOT NULL,
+    [Movil]     NVARCHAR (50)  NOT NULL,
+    [Password]  NVARCHAR (50)  NOT NULL,
+    [City]      NVARCHAR (70)  NOT NULL,
+    [Country]   NVARCHAR (70)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
